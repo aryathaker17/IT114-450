61c2f483f0fb0720ee9ba542ad6681e0b5b5759a
aryathaker17 <aryathaker7@gmail.com>
2022-07-02 09:53:08 -0400

    Practical UI file

--- BasicGUI/MyFrame.java
+++ BasicGUI/MyFrame.java
@@ -9,12 +9,10 @@ public class MyFrame extends JFrame {
 		this.setTitle(myTitle);
 		JTextField textField = new JTextField("Hello World");
 		textField.setSize(100, 100);//define the size in pixels
-		add(textField);//add the JTextField to the hierarchy and make it 
-visible to the user
+		add(textField);//add the JTextField to the hierarchy and make it visible to the user
 		setLayout(null);//no layout manager for now
 		setSize(200, 200);//define the size in pixels
-		setVisible(true);//required to show the window; otherwise 
-program terminates
+		setVisible(true);//required to show the window; otherwise program terminates
 	}
 
 	public static void main(String[] args) {
